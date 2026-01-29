bf181a0a9c121e42a5f2b9340ad9e7e1025fe7fa
Ronin619 <eytam619@gmail.com>
2026-01-28 21:41:09 -0500

    install yup and implement yup resolver

--- app/components/Form.js
+++ app/components/Form.js
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { fetchWeatherData } from '../store/slices/weather'
 import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
 
 export default function Form() {
   const {
     register,
     handleSubmit,
     formState: { error },
-  } = useForm()
+  } = useForm({
+    resolver: yupResolver(),
+  })
   const dispatch = useDispatch()
   const [city, setCity] = useState('')
 
